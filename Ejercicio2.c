af13f1660eaa017629bc766d3bfc1958e2ef099d
Narbis Tamiche <54629185+NarbisCOD@users.noreply.github.com>
2020-06-13 20:24:58 -0300

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -6,7 +6,7 @@ const int MAX_USUARIO = 0;
 const int MAX_CLAVE = 10;
 const int DEF_CLAVE = -1;
 const int MAX_INTENTOS =  5;
-const int CORREGIR_INTENTOS = 0;
+const int CONTROL_INTENTOS = 4;
 
 
 const int CLAVE_1 = 123;
@@ -25,17 +25,17 @@ int main(){
     int i =0;
    
     for ( i; i < MAX_INTENTOS; i++){
-        printf("Ingrese nombre de usuario -Tienes %i intentos para acceder:\n", (MAX_INTENTOS - i - CORREGIR_INTENTOS));
+        printf("Ingrese nombre de usuario -Tienes %i intentos para acceder:\n", (MAX_INTENTOS - i));
         scanf("%s", &usuario);
         printf("Ingrese su Clave:\n");
         scanf("%i", &clave_ingresada);        
             if (((strcmp(usuario, usuario_1)==0) && (clave_ingresada == CLAVE_1)) || ((strcmp(usuario, usuario_2)==0) && (clave_ingresada == CLAVE_2)) || ((strcmp(usuario, usuario_3)==0) && (clave_ingresada == CLAVE_3))){
                 printf("Bienvenido %s :)", usuario);
                 i = MAX_INTENTOS;
-                }else if(i==4){
-                    printf("Espere un dia para volver a intentarlo");
-                }else{
+                }else if(i < CONTROL_INTENTOS){
                     printf("Usuario y/o clave errada :< \n");
+                }else{
+                    printf("Espere un dia para volver a intentarlo");
                 } 
     }
     return 0;
